d05c9b8ee2c4928306d6628d5685d85ca6ff216c
Tom Watts <qlxt9s9s@duck.com>
2023-09-22 15:36:29 -0700

    Using extension template from extension creating program and adding some initial icon setup

--- extension.js
+++ extension.js
@@ -1,32 +1,75 @@
 'use strict';
 
-// This is a handy import we'll use to grab our extension's object
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+
+const { Gio, GObject, St } = imports.gi;
+
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
 
-// This function is called once when your extension is loaded, not enabled. This
-// is a good time to setup translations or anything else you only do once.
-//
-// You MUST NOT make any changes to GNOME Shell, connect any signals or add any
-// MainLoop sources here.
+var TimeToLogIndicator = GObject.registerClass(
+    class TimeToLogIndicator extends PanelMenu.Button {
+        _init() {
+            super._init(0.0, `${Me.metadata.name} Indicator`, false);
+
+            // Pick an icon
+            this.icon = new St.Icon({
+                style_class: 'system-status-icon',
+                icon_name: 'editor-symbolic'
+            });
+            this.actor.add_child(this.icon);
+
+            this.click = this.connect("button-release-event", this._createLogFile.bind(this));
+        }
+
+        _createLogFile() {
+        }
+    }
+);
+
+var indicator = null;
+
+class Extension {
+    constructor() {
+    }
+
+    enable() {
+        log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+
+        indicator = new TimeToLogIndicator();
+
+        Main.panel.addToStatusArea(`${Me.metadata.name} Indicator`, indicator);
+    }
+
+    disable() {
+        log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+
+        if (indicator !== null) {
+            indicator.destroy();
+            indicator = null;
+        }
+    }
+}
+
 function init() {
-    log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
-}
-
-// This function could be called after your extension is enabled, which could
-// be done from GNOME Tweaks, when you log in or when the screen is unlocked.
-//
-// This is when you setup any UI for your extension, change existing widgets,
-// connect signals or modify GNOME Shell's behaviour.
-function enable() {
-    log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
-}
-
-// This function could be called after your extension is uninstalled, disabled
-// in GNOME Tweaks, when you log out or when the screen locks.
-//
-// Anything you created, modifed or setup in enable() MUST be undone here. Not
-// doing so is the most common reason extensions are rejected during review!
-function disable() {
-    log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+    return new Extension();
 }
